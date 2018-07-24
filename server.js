3f9d09d3612453bc5fb71a806b5841e0900413f8
WCPeragine <ertyjunior@gmail.com>
2018-07-24 18:36:57 +0000

    changed host & ssl

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-reticulated-33737',
-    user : 'chance',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 

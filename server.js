04ed81e200522af90c1e163961ea8c2306b065e7
WCPeragine <ertyjunior@gmail.com>
2018-07-24 18:32:57 +0000

    changed to heroku server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-reticulated-33737',
     user : 'chance',
     password : '',
     database : 'smart-brain'
